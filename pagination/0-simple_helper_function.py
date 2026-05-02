7b899fdc45cd7295a456fd3b03658a0a563af631
Sahila001 <sahilamikayilova001@gmail.com>
2026-05-02 13:56:32 +0000

    Add simple helper function for pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-"""
-Module that provides a helper function for pagination
-"""
 
 
 def index_range(page: int, page_size: int) -> tuple:
-
     start_index = (page - 1) * page_size
     end_index = page * page_size
-
     return (start_index, end_index)
