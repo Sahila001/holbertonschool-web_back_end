32d686a2d4cab065bf2f10c70911fa28335ed066
Sahila001 <sahilamikayilova001@gmail.com>
2026-05-02 14:03:56 +0000

    Implement hypermedia pagination with get_hyper method

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        dataset = self.dataset()
+        start, end = index_range(page, page_size)
+
+        if start >= len(dataset):
+            return []
+
+        return dataset[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        data = self.get_page(page, page_size)
+        dataset_len = len(self.dataset())
+
+        total_pages = math.ceil(dataset_len / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
